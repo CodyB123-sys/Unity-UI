d06c33d17e6eb4b611e53b33aa984c9b9a8be31c
Cody Beinert <codybeinert123@gmail.com>
2021-06-15 11:34:47 -0400

    create OptionsMenuEnter animation

--- New Unity Project/Assets/Scripts/OptionsScript.cs
+++ New Unity Project/Assets/Scripts/OptionsScript.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class OptionsScript : MonoBehaviour
 {
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
